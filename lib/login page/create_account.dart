00199a6196ec44e36b9de695d65abd83a674b546
Elijah Thompson <ssomething511@gmail.com>
2025-03-05 03:22:06 -0500

    Moved login page builder.
    Moved the log in page to a separate dart file to clear up Main. Next up is adding routes.

---
+++ lib/login page/create_account.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class CreateAccount extends StatelessWidget {
+  const CreateAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 160, 0, 0),
+                child: Text(
+                  "Take-a-Bite",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 42),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
+                child: Text(
+                  "Create an Account", 
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 4, 0, 0),
+                child: Text(
+                  "Enter your email to sign up for this app", 
+                  style: TextStyle(fontSize: 14),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 24, 0, 0),
+                child: SizedBox(
+                  width: 300,
+                  child: TextField(
+                    decoration: 
+                      InputDecoration(border: OutlineInputBorder(),
+                      labelText: "Email Address",
+                      labelStyle: TextStyle(fontSize: 14),
+                      contentPadding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                child: Container(
+                  width: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                  child: TextButton(
+                    style: const ButtonStyle(
+                      foregroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 255, 255, 255)),
+                      backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 0, 0, 0)),
+                      shape: WidgetStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8)))),
+                    ),
+                    onPressed: () => {},
+                    child: const Text("Continue"),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 225, 0),
+                child: TextButton(
+                  onPressed: () => {},
+                  child: const Text(
+                    "Signing in?", 
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontStyle: FontStyle.italic,
+                      color: Color.fromRGBO(33, 148, 255, 1),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,19 @@
 import 'package:flutter/material.dart';
+import './login page/create_account.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Take-a-Bite",
       theme: ThemeData(scaffoldBackgroundColor: const Color.fromRGBO(255, 255, 255, 1)), // Background color of Main App
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.fromLTRB(0, 160, 0, 0),
-                child: Text(
-                  "Take-a-Bite",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 42),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
-                child: Text(
-                  "Create an Account", 
-                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.fromLTRB(0, 4, 0, 0),
-                child: Text(
-                  "Enter your email to sign up for this app", 
-                  style: TextStyle(fontSize: 14),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.fromLTRB(0, 24, 0, 0),
-                child: SizedBox(
-                  width: 300,
-                  child: TextField(
-                    decoration: 
-                      InputDecoration(border: OutlineInputBorder(),
-                      labelText: "Email Address",
-                      labelStyle: TextStyle(fontSize: 14),
-                      contentPadding: EdgeInsets.fromLTRB(16, 0, 16, 0),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-                child: Container(
-                  width: 300,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(0),
-                  ),
-                  child: TextButton(
-                    style: const ButtonStyle(
-                      foregroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 255, 255, 255)),
-                      backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 0, 0, 0)),
-                      shape: WidgetStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8)))),
-                    ),
-                    onPressed: () => {},
-                    child: const Text("Continue"),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 225, 0),
-                child: TextButton(
-                  onPressed: () => {},
-                  child: const Text(
-                    "Signing in?", 
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontStyle: FontStyle.italic,
-                      color: Color.fromRGBO(33, 148, 255, 1),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: const CreateAccount(),
     );
   }
 }
