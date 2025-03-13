7c01ad953c8ea665ba4b52ee914933859a9a600d
Elijah Thompson <ssomething511@gmail.com>
2025-03-13 17:58:09 -0400

    Added Card widget for Search Page.
    Created a card widget for each recipe displayed on a search page. Filled with placeholder info for now.

--- lib/nav_pages/search.dart
+++ lib/nav_pages/search.dart
@@ -6,7 +6,46 @@ class Search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text("Search/Home Page")),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 48, 0, 0),
+              child: Text("Search/Home Page (this would be search bar)"),
+            ),
+            RecipeCard(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Recipe displayed on Search Page
+class RecipeCard extends StatelessWidget {
+  const RecipeCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const FractionallySizedBox(
+      widthFactor: 0.9,
+      child: Card(
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(8.0),
+                topRight: Radius.circular(8.0),
+              ),
+              child: Image(image: AssetImage('lib/imgs/pizza.jpg'))
+            ),
+            ListTile(
+              title: Text("How to make a pizza in 10 minutes"),
+              subtitle: Text("15 mins | 4 - 5 people"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
