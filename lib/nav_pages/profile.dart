674e006cd22ef13b3774050aa9e9465f295f1234
Elijah Thompson <ssomething511@gmail.com>
2025-03-22 17:09:11 -0400

    Added Tabs to profile.
    Added two tabs for the profile, created recipes and saved recipes. Currently don't have recipes but display grids of items to scroll through.

--- lib/nav_pages/profile.dart
+++ lib/nav_pages/profile.dart
@@ -17,6 +17,10 @@ class Profile extends StatelessWidget {
               padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
               child: ProfileDescription(),
             ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
+              child: RecipeList(),
+            ),
           ],
         ),
       ),
@@ -37,7 +41,12 @@ class ProfileHead extends StatelessWidget {
           decoration: const BoxDecoration(
             color: Colors.white,
             shape: BoxShape.circle,
-            boxShadow: [BoxShadow(blurRadius: 2, color: Color.fromARGB(255, 107, 107, 107), spreadRadius: 1)],
+            boxShadow: [
+              BoxShadow(
+                  blurRadius: 2,
+                  color: Color.fromARGB(255, 107, 107, 107),
+                  spreadRadius: 1)
+            ],
           ),
           child: const CircleAvatar(
             radius: 50,
@@ -50,20 +59,16 @@ class ProfileHead extends StatelessWidget {
         const Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              "CheeseLover",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-              )
-            ),
-            Text(
-              "@CheeseBros",
-              style: TextStyle(
-                fontWeight: FontWeight.w300,
-                fontSize: 14,
-              )
-            ),
+            Text("CheeseLover",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                )),
+            Text("@CheeseBros",
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 14,
+                )),
             Text(
               "0 Followers  0 Recipes",
               style: TextStyle(
@@ -89,6 +94,81 @@ class ProfileDescription extends StatelessWidget {
       child: const Text(
         "Cooking Legend.\nSeeking cheese recipes night and day.\n#CheeseLovers25",
         style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
+// Will list created or saved recipes depending on which is selected. Defaults to created.
+class RecipeList extends StatefulWidget {
+  const RecipeList({super.key});
+
+  @override
+  State<RecipeList> createState() => _RecipeListState();
+}
+
+class _RecipeListState extends State<RecipeList> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Column(
+        children: [
+          const TabBar(
+            tabs: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
+                child: Text(
+                  "Created",
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
+                child: Text(
+                  "Saved",
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 363, // Maybe find some way to be dynamic? Can't not have set height or TabBarView doesn't display.
+            child: TabBarView(
+              children: [
+                // First tab: Created Recipes
+                GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 3,
+                  children: List.generate(200, (index) {
+                    return Center(
+                      child: Text(
+                        'Item $index',
+                      ),
+                    );
+                  }),
+                ),
+                // Second tab: Saved Recipes
+                GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 3,
+                  children: List.generate(100, (index) {
+                    return Center(
+                      child: Text(
+                        'Item $index',
+                      ),
+                    );
+                  }),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
