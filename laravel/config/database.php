2ca990afd95db2a487a77de6b1cf965d6450c3f9
Elijah Thompson <ssomething511@gmail.com>
2025-03-27 21:01:19 -0400

    Changed host and post
    Changed host and ports for connection testing. Successfully connected to database and created tables.

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -44,13 +44,13 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
+            //'url' => env('DB_URL'),
             //'url' => env('DB_HOST'),
             //'host' => env('DB_HOST', '127.0.0.1'),
             'host' => env('DB_HOST', '3.93.61.3'),
             //'host' => '127.0.0.1',
-            'port' => env('DB_PORT', '22'),
-            //'port' => env('DB_PORT', '3306'),
+            //'port' => env('DB_PORT', '22'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'Senior_Project'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'seniorprojectspring2025'),
