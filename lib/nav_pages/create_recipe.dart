53d7b1441261870c850abac506fde839e3a04c83
Elijah Thompson <ssomething511@gmail.com>
2025-04-02 19:55:38 -0400

    Added a few more fields for Create page form.
    Added fields for the subtitle, prep time, cook time, and serving size of a recipe to the form of the Create Recipe page. The results are reflected in the dialog from the submit button for now.
    
    Want to add a preview button for the recipe. Will add instructions to the form next, but need to wait for database connection for ingredients.

--- lib/nav_pages/create_recipe.dart
+++ lib/nav_pages/create_recipe.dart
@@ -10,31 +10,161 @@ class CreateRecipe extends StatefulWidget {
 // Rough form as base for page (for now)
 class _CreateRecipeState extends State<CreateRecipe> {
   final formKey = GlobalKey<FormState>();
-  final titleController = TextEditingController();
 
+  // Controllers to get values from form
+  final titleController = TextEditingController();
+  final subtitleController = TextEditingController();
+  final prepController = TextEditingController();
+  final cookController = TextEditingController();
+  final servingSizeController = TextEditingController();
+
+  // Image selector somewhere??
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 0.9,
-      heightFactor: 0.9,
-      child: Form(
-        key: formKey,
-        child: Column(
-          children: [
-            // Title of Recipe
-            TextFormField(
-              controller: titleController,
-              decoration: const InputDecoration(
-                labelText: "Title *",
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 48, 0, 0),
+        child: Form(
+          key: formKey,
+          child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 24, 0, 4),
+              child: Text(
+                "Create a Recipe",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-              validator: (String? value) {
-                return (value != null && value != "") ? null : "A title is required.";
-              }
+            ),
+            const Text(
+              "Enter the Recipe's information below",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            const Divider(
+              indent: 50,
+              endIndent: 50,
+            ),
+            // Title of Recipe
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0,16,0,16),
+              child: FractionallySizedBox(
+                widthFactor: 0.5,
+                child: TextFormField(
+                    controller: titleController,
+                    textCapitalization: TextCapitalization.sentences,
+                    decoration: const InputDecoration(
+                      labelText: "Title *",
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (String? value) {
+                      return (value != null && value != "")
+                          ? null
+                          : "A title is required.";
+                    }),
+              ),
             ),
             // Subtitle/text
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0,0,0,16),
+              child: FractionallySizedBox(
+                widthFactor: 0.8,
+                child: TextFormField(
+                    controller: subtitleController,
+                    textCapitalization: TextCapitalization.sentences,
+                    minLines: 1,
+                    maxLines: 5,
+                    decoration: const InputDecoration(
+                      labelText: "Subtitle *",
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (String? value) {
+                      return (value != null && value != "")
+                          ? null
+                          : "A subtitle is required.";
+                    }),
+              ),
+            ),
             // Ingredients (proportions too)
-            // Prep Time / Cook Time / Serving Size
+            // Prep Time
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0,0,0,16),
+              child: FractionallySizedBox(
+                widthFactor: 0.4,
+                child: TextFormField(
+                  controller: prepController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: "Prep Time (mins) *",
+                    labelStyle: TextStyle(
+                      fontSize: 12,
+                    ),
+                    border: OutlineInputBorder(),
+                  ),
+                  style: const TextStyle(
+                      //fontSize: 16,
+                      ),
+                  validator: (String? value) {
+                    return (value != null && value != "")
+                        ? null
+                        : "Prep Time must be specified.";
+                  }
+                ),
+              ),
+            ),
+            // Cook Time
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0,0,0,16),
+              child: FractionallySizedBox(
+                widthFactor: 0.4,
+                child: TextFormField(
+                  controller: cookController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: "Cook Time (mins) *",
+                    border: OutlineInputBorder(),
+                    labelStyle: TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                  validator: (String? value) {
+                    return (value != null && value != "")
+                        ? null
+                        : "Cook Time must be specified.";
+                  }
+                ),
+              ),
+            ),
+            // Serving Size
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0,0,0,16),
+              child: FractionallySizedBox(
+                widthFactor: 0.4,
+                child: TextFormField(
+                  controller: servingSizeController,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                    labelText: "Serving Size *",
+                    border: OutlineInputBorder(),
+                    labelStyle: TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                  validator: (String? value) {
+                    return (value != null && value != "")
+                        ? null
+                        : "Serving Size must be specified.";
+                  }
+                ),
+              ),
+            ),
             // Instructions
+            // Create/Submit Button
             ElevatedButton(
               onPressed: () {
                 if (formKey.currentState!.validate()) {
@@ -51,6 +181,10 @@ class _CreateRecipeState extends State<CreateRecipe> {
                             const SizedBox(height: 15),
                             const Text("Current Values:"),
                             Text("Title: ${titleController.text}"),
+                            Text("Subtitle: ${subtitleController.text}"),
+                            Text("Prep Time: ${prepController.text}"),
+                            Text("Cook Time: ${cookController.text}"),
+                            Text("Serving Size: ${servingSizeController.text}"),
                             TextButton(
                               onPressed: () {
                                 Navigator.pop(context);
@@ -66,7 +200,7 @@ class _CreateRecipeState extends State<CreateRecipe> {
               },
               child: const Text("Submit"),
             ),
-          ]
+          ]),
         ),
       ),
     );
